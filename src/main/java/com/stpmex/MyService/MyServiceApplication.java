60ba4be74d3495c931011e4be11a56fa924aa565
rubeng <ruben.gallegos@stpmex.com>
2025-08-21 12:16:58 -0600

    Update MyServiceApplication.java

--- src/main/java/com/stpmex/MyService/MyServiceApplication.java
+++ src/main/java/com/stpmex/MyService/MyServiceApplication.java
@@ -1,13 +1,17 @@
 package com.stpmex.MyService;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@Slf4j
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MyServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyServiceApplication.class, args);
+		log.info("Dummy service started");
 	}
 
 }
